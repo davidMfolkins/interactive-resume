78a0e4c3884a8c1ef22a20bc10be676065c0f5be
David Folkins <davidf@EXS-Programmer11.local>
2021-07-12 14:16:58 -0400

    make nav bar sticky

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,26 +6,32 @@ function Projects() {
     <div className="projects">
       <div className="project-containers">
         <img className="project-pic" src={profilepic} width="250px" alt="profile" />
+        <h3>Project title</h3>
         <span className="project-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
       </div>
       <div className="project-containers">
         <img className="project-pic" src={profilepic} width="250px" alt="profile" />
+        <h3>Project title</h3>
         <span className="project-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
       </div>
       <div className="project-containers">
         <img className="project-pic" src={profilepic} width="250px" alt="profile" />
+        <h3>Project title</h3>
         <span className="project-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
       </div>
       <div className="project-containers">
         <img className="project-pic" src={profilepic} width="250px" alt="profile" />
+        <h3>Project title</h3>
         <span className="project-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
       </div>
       <div className="project-containers">
         <img className="project-pic" src={profilepic} width="250px" alt="profile" />
+        <h3>Project title</h3>
         <span className="project-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
       </div>
       <div className="project-containers">
         <img className="project-pic" src={profilepic} width="250px" alt="profile" />
+        <h3>Project title</h3>
         <span className="project-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
       </div>
     </div>
