c47d851b501e7a18d1d86d1b59880e81f8529b68
David Folkins <davidf@EXS-Programmer11.local>
2021-07-12 10:34:35 -0400

    add links to github and linkedin

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,6 +9,8 @@ function Nav() {
       <Link className="nav-item" to="/resume">Resume</Link>
       <Link className="nav-item" to="/projects">Projects</Link>
       <Link className="nav-item" to="/contact">Contact</Link>
+      <a className="nav-item" target="_blank" href="https://github.com/davidMfolkins">Github</a>
+      <a className="nav-item" target="_blank" href="https://ca.linkedin.com/in/davidfolkins">Linkedin</a>
     </div>
   );
 }
