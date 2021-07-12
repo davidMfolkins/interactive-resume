cf12c34c27e2e6d5d0b0cee53c99bbf70438612c
David Folkins <davidf@EXS-Programmer11.local>
2021-07-12 13:16:54 -0400

    add css and media query to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,12 @@ import './Contact.css'
 
 function Contact() {
   return (
-    <div>
-      Contact
+    <div className="contact">
+      <span className="contact-item">Email: david.folkins88@gmail.com</span>
+      <span className="contact-item">Linkedin: <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://ca.linkedin.com/in/davidfolkins">david.folkins</a></span>
     </div>
   );
 }
